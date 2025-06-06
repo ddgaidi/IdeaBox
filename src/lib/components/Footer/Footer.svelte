a2a958d99701058933a4fa66cfea71ec83fefdd5
Djamel G <90888178+ddgaidi@users.noreply.github.com>
2025-06-06 14:43:22 +0200

    code push

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <footer class="relative flex items-center justify-center z-20 py-4 text-center text-gray-600 bg-gray-100 border-t border-gray-200">
-	<a href="https://github.com/ddgaidi" class="hover:scale-105 transition-transform duration-300" target="_blank">
+	<a href="https://github.com/ddgaidi" class="hover:scale-105 mr-4 transition-transform duration-300" target="_blank">
 	<img
 		src={pp}
 		alt=""
-		class="w-10 mr-4"
+		class="w-10"
 		oncontextmenu={e => e.preventDefault()}
 		draggable={false}
 	/>
