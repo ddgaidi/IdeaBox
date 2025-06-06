4c6322bf955ecfbc72e78c12833df83742f8b765
Djamel G <90888178+ddgaidi@users.noreply.github.com>
2025-06-06 14:35:38 +0200

    code push

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,3 +1,14 @@
-<footer class="relative z-20 py-4 text-center text-gray-600 bg-gray-100 border-t border-gray-200">
+<script>
+	import pp from '$lib/images/pp.png';
+</script>
+
+<footer class="relative flex items-center justify-center z-20 py-4 text-center text-gray-600 bg-gray-100 border-t border-gray-200">
+	<img
+		src={pp}
+		alt=""
+		class="w-10 mr-4"
+		oncontextmenu={e => e.preventDefault()}
+		draggable={false}
+	/>
 	<p>&copy; {new Date().getFullYear()} <span class="text-blue-600 font-bold">IdeaBox</span> Tous droits réservés.</p>
 </footer>
